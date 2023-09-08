3fa362a4a3159d97a137251d3e5f220359fe0d45
Nikita-Schneider <nikitasch2001@gmail.com>
2023-09-08 09:35:13 +0200

    .

--- summary_11/scripts/main.js
+++ summary_11/scripts/main.js
@@ -3,3 +3,14 @@
 
 
 //2. Делаем блок на странице, который будет запускать отложенное выполнение. Нужно сделать инпут, в который будем вводить задержку в секундах, и кнопку "Запуск". При нажатии на кнопку "Запуск", нужно через N секунд вывести сообщение "Delayed message". N - это кол-во секунд, введенное в инпуте.
+
+const delayInput = document.querySelector('#delayTime');
+const delayBtn = document.querySelector('#delayStart');
+
+delayBtn.addEventListener('click', () => {
+    const delayTime = delayInput.value;
+    if (delayTime) {
+        setTimeout( () => {
+            console.log('Delayed by ' + delayTime);
+        }, delayTime * 1000);tex
+});
