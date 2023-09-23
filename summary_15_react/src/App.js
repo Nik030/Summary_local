a2e380d4f96cd5eca613c0a3e8af1e5f051fac8e
Nikita-Schneider <nikitasch2001@gmail.com>
2023-09-23 21:41:48 +0200

    .

--- summary_15_react/src/App.js
+++ summary_15_react/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Hello World from React</p>
+      <p>Hello World from React! {new Date().toLocaleString()}</p>
     </div>
   );
 }
